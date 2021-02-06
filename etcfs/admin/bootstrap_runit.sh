c151b240efe3c6b6de3d8f6e758aa02ce962965f
Pete Birkinshaw <pete@binary-ape.org>
2021-02-06 21:25:59 +0000

    Replace runit bootstrap script with something that knows how to shutdown services

--- etcfs/admin/bootstrap_runit.sh
+++ etcfs/admin/bootstrap_runit.sh
@@ -1,10 +1,52 @@
-#!/usr/bin/env bash
+#!/bin/sh
+## Script by Sanj Ambalavanar
+##   (https://sanjeevan.co.uk/blog/running-services-inside-a-container-using-runit-and-alpine-linux)
+## Adjusted to work on Debian
 
-## Import envvars dumped by Dockerfile, since runit starts with no ENV
+shutdown() {
+  echo "Shutting down services..."
+
+  ## First shutdown any service started by runit
+  for _srv in $(ls -1 /etc/service); do
+    sv force-stop $_srv
+  done
+
+  ## Shutdown runsvdir command
+  kill -HUP $RUNSVDIR
+  wait $RUNSVDIR
+
+  # give processes time to stop
+  sleep 0.5
+
+  # kill any other processes still running in the container
+  for _pid  in $(ps -eo pid | grep -v PID  | tr -d ' ' | grep -v '^1$' | head -n -6); do
+    timeout -t 5 /bin/sh -c "kill $_pid && wait $_pid || kill -9 $_pid"
+  done
+  exit
+}
+
+# Store environment variables
 export > /etc/envvars
 
-## A bin path would be rather useful too
 PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin
 
-## Run it
-exec env - PATH=$PATH runsvdir -P /etc/service
+## Run all scripts in the run_once folder. Once.
+/bin/run-parts /etc/run_once
+
+## Start the actual services
+exec env - PATH=$PATH runsvdir -P /etc/service &
+
+RUNSVDIR=$!
+echo "Started runsvdir, PID is $RUNSVDIR"
+echo "Waiting for processes to start...."
+
+sleep 5
+for _srv in $(ls -1 /etc/service); do
+    sv status $_srv
+done
+
+## Catch shutdown signals and run the shutdown function
+trap shutdown TERM HUP QUIT INT
+wait $RUNSVDIR
+
+shutdown
